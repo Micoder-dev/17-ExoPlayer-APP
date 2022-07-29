7687e26ce8b9a51a1c3a5a6291df1e3dda38c001
Micoder-dev <micoder.com@gmail.com>
2022-07-30 00:09:03 +0530

    Exoplayer Setup - Play your first music

--- app/src/main/java/com/example/exoplayerapp/MainActivity.java
+++ app/src/main/java/com/example/exoplayerapp/MainActivity.java
@@ -2,13 +2,34 @@ package com.example.exoplayerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.net.Uri;
 import android.os.Bundle;
 
+import com.google.android.exoplayer2.MediaItem;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.ui.PlayerView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private PlayerView mPlayerView;
+    private SimpleExoPlayer mSimpleExoPlayer;
+    private String music_url = "https://opengameart.org/sites/default/files/Cyberpunk%20Moonlight%20Sonata_0.mp3";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPlayerView = findViewById(R.id.my_player_view);
+
+        mSimpleExoPlayer  = new SimpleExoPlayer.Builder(this).build();
+
+        mPlayerView.setPlayer(mSimpleExoPlayer);
+
+        MediaItem mediaItem = MediaItem.fromUri(Uri.parse(music_url));
+        mSimpleExoPlayer.setMediaItem(mediaItem);
+        mSimpleExoPlayer.prepare();
+        mSimpleExoPlayer.play();
+
     }
 }
