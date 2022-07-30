e0ed422c2686def596d95144daff273852fa61c9
Micoder-dev <micoder.com@gmail.com>
2022-07-30 21:07:17 +0530

    Create music play list

--- app/src/main/java/com/example/exoplayerapp/MainActivity.java
+++ app/src/main/java/com/example/exoplayerapp/MainActivity.java
@@ -13,7 +13,17 @@ public class MainActivity extends AppCompatActivity {
 
     private PlayerView mPlayerView;
     private SimpleExoPlayer mSimpleExoPlayer;
-    private String music_url = "https://opengameart.org/sites/default/files/Cyberpunk%20Moonlight%20Sonata_0.mp3";
+//    private String music_url = "https://opengameart.org/sites/default/files/Cyberpunk%20Moonlight%20Sonata_0.mp3";
+
+    private String[] music_list = new String[] {
+
+            "https://opengameart.org/sites/default/files/song18_0.mp3",
+            "https://opengameart.org/sites/default/files/TownTheme.mp3",
+            "https://opengameart.org/sites/default/files/battleThemeA.mp3",
+            "https://opengameart.org/sites/default/files/Heroic%20Demise%20%28New%29_0.mp3",
+            "https://opengameart.org/sites/default/files/the_field_of_dreams.mp3"
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +36,30 @@ public class MainActivity extends AppCompatActivity {
 
         mPlayerView.setPlayer(mSimpleExoPlayer);
 
-        MediaItem mediaItem = MediaItem.fromUri(Uri.parse(music_url));
-        mSimpleExoPlayer.setMediaItem(mediaItem);
+        for (String music_url : music_list) {
+
+            MediaItem mediaItem = MediaItem.fromUri(Uri.parse(music_url));
+
+            mSimpleExoPlayer.addMediaItem(mediaItem);
+
+        }
+
+      //  mSimpleExoPlayer.setMediaItem(mediaItem);
         mSimpleExoPlayer.prepare();
         mSimpleExoPlayer.play();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (mSimpleExoPlayer != null) {
+
+            mSimpleExoPlayer.release();
+            mSimpleExoPlayer = null;
+
+        }
 
     }
 }
